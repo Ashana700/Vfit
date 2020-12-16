6b4da16aaf993b1318922cbf87a56bcf546eec8d
aayu1401 <aayushi_m@me.iitr.ac.in>
2020-12-16 21:11:19 +0530

    created by aayu

---
+++ Vfit/app/src/main/java/com/example/vfit/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.vfit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView= findViewById(R.id.nav_bar);
+        bottomNavigationView.setSelectedItemId(R.id.feed);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.exercise:
+                        startActivity(new Intent(getApplicationContext(),Exercise.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.feed:
+                        return true;
+                    case R.id.connect:
+                        startActivity(new Intent(getApplicationContext(),Connect.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(),Profile.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+}
