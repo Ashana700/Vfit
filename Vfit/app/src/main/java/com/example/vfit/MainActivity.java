7ca9c68b2b3672023dca25bdcfbdcec243b54164
Alazord <Alazord@users.noreply.github.com>
2020-12-20 02:11:37 +0530

    First upload

--- Vfit/app/src/main/java/com/example/vfit/MainActivity.java
+++ Vfit/app/src/main/java/com/example/vfit/MainActivity.java
@@ -16,31 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView= findViewById(R.id.nav_bar);
-        bottomNavigationView.setSelectedItemId(R.id.feed);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId())
-                {
-                    case R.id.exercise:
-                        startActivity(new Intent(getApplicationContext(),Exercise.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.feed:
-                        return true;
-                    case R.id.connect:
-                        startActivity(new Intent(getApplicationContext(),Connect.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.profile:
-                        startActivity(new Intent(getApplicationContext(),Profile.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                }
-                return false;
-            }
-        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
+
+
     }
 
 }
